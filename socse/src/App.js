51660ccc4f3a6f3e5a418ae4fa2b1c24b4bf45b8
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-11-30 22:39:15 +0530

    images and download

--- socse/src/App.js
+++ socse/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import myImage from './logo.png';
 import events from './images/events.jpg';
 import sports from './images/sports.jpg';
+import literature from './images/literature.jpg';
+import technical from './images/techical.jpg';
+import cocurricular from './images/co-curricular.png';
+import  achievements from './images/acheivements.jpg';
+import  download from './images/download.png';
 
 import './App.css';
 
 function App() {
   return (
+    <>
     <header>
     <div class="navbar">
     <img src={myImage} alt="Description" class="logo"/>
@@ -18,10 +24,25 @@ function App() {
             <li><a>Contact Us</a></li>
         </ul>
     </nav>
-</div>
-  
+</div> 
 </header>
 
+<body>
+  <div class="images">
+<img src={events} alt="Description" class="events"/>
+<img src={literature} alt="Description" class="literature"/>
+<img src={cocurricular} alt="Description" class="co-curricular"/>
+<img src={sports} alt="Description" class="sports"/>
+<img src={technical} alt="Description" class="technical"/>
+<img src={achievements} alt="Description" class="achievements"/>
+</div>
+</body>
+
+<footer>
+<img src={download} alt="Description" class="download"/>
+<button className='download-text'><b>Download</b></button>
+</footer>
+</>
 
 
 
