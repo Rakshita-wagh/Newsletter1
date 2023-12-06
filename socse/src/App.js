8a61264111c59034e7bb8e010c7c7e6440a84cd4
Srajana20 <srajanavnaik@gmail.com>
2023-12-06 15:08:08 +0530

    doneee

--- socse/src/App.js
+++ socse/src/App.js
@@ -1,69 +1,25 @@
-import logo from './logo.svg';
-import myImage from './logo.png';
-import events from './images/events.jpg';
-import sports from './images/sports.jpg';
-import literature from './images/literature.jpg';
-import technical from './images/techical.jpg';
-import cocurricular from './images/co-curricular.png';
-import  achievements from './images/acheivements.jpg';
-import  download from './images/download.png';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 
+function App(){
+  return(
+    <div>
+      <BrowserRouter>
+      <Home/>
+      <Routes>
+      <Route path="/home" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/home" element={<Contact/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
 
-
-import './App.css';
-
-function App() {
-
-  return (
-    
-    <>
-   
-    <header>
-    <div class="navbar">
-    <img src={myImage} alt="Description" class="logo"/>
-    <h2 class="heading">Socse Association</h2>
-    <nav>
-     
-        <ul>
-            <li><a>Home</a></li>
-            <li><a>About Us</a></li>
-            <li><a>Contact Us</a></li>
-        </ul>
-       
-    </nav>
-</div> 
-</header>
-
-
-
-<body>
-  <div class="images">
-<img src={events} alt="Description" class="events"/>
-
-
-<img src={literature} alt="Description" class="literature"/>
-
-
-
-
-<img src={cocurricular} alt="Description" class="co-curricular"/>
-<img src={sports} alt="Description" class="sports"/>
-<img src={technical} alt="Description" class="technical"/>
-<img src={achievements} alt="Description" class="achievements"/>
-</div>
-</body>
-
-
-<footer>
-<img src={download} alt="Description" class="download"/>
-<button className='download-text'><b>Download</b></button>
-</footer>
-</>
-
-
-
-  );
 }
 
 export default App;
---
+++ socse/src/Pages/Home.js
@@ -0,0 +1,66 @@
+
+import myImage from '../logo.png';
+import events from '../images/events.jpg';
+import sports from '../images/sports.jpg';
+import literature from '../images/literature.jpg';
+import technical from '../images/techical.jpg';
+import cocurricular from '../images/co-curricular.png';
+import achievements from '../images/acheivements.jpg';
+import download from '../images/download.png';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Literature from '../modules/Literature';
+
+import './Home.css';
+
+
+
+
+function Home() {
+  return (
+    
+    
+    // <BrowserRouter>
+      <>
+        <header>
+          <div className="navbar">
+            <img src={myImage} alt="Description" className="logo" />
+            <h2 className="heading">Socse Association</h2>
+            <nav>
+              <ul>
+              <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <body>
+          <div className="images">
+            <img src={events} alt="Description" className="events" />
+            {/* <Link to="/Literature" target="_blank">
+              <img src={literature} alt="Description" className="literature" />
+            </Link> */}
+            <img src={cocurricular} alt="Description" className="co-curricular" />
+            <img src={sports} alt="Description" className="sports" />
+            <img src={technical} alt="Description" className="technical" />
+            <img src={achievements} alt="Description" className="achievements" />
+          </div>
+       
+        </body>
+
+        <footer>
+          <img src={download} alt="Description" className="download" />
+          <button className='download-text'><b>Download</b></button>
+        </footer>
+{/* <Routes>
+  <Route path='/literature' element={<Literature/>}></Route>
+</Routes> */}
+        
+      </>
+    // </BrowserRouter>
+  );
+}
+
+export default Home;
