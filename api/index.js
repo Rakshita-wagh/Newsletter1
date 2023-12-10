cf67b574b74ddb0c857b0e89ef6c988717791aa3
vaishnavicg <01fe21bcs271@kletech.ac.in>
2023-12-10 14:49:39 +0530

    connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv  from 'dotenv';
+dotenv.config();
+const uri = 'mongodb+srv://vaishnavi:vaish@socse.yxckbab.mongodb.net/your-database-name?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app =express();
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
