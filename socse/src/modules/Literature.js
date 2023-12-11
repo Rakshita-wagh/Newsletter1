81ed3e7fa83d809c9a7e18a793c8fcc9d8132cc8
Srajana20 <srajanavnaik@gmail.com>
2023-12-11 20:30:11 +0530

    add

--- socse/src/modules/Literature.js
+++ socse/src/modules/Literature.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Literature.css';
+
+
 function Literature() {
+  const [selectedYearPoems, setSelectedYearPoems] = useState('');
+  const [selectedYearArticles, setSelectedYearArticles] = useState('');
+
+  const handlePoemsChange = (event) => {
+    setSelectedYearPoems(event.target.value);
+  };
+
+  const handleArticlesChange = (event) => {
+    setSelectedYearArticles(event.target.value);
+  };
+
   
   return (
     <div className="literature-page">
-      <h1>Literature Events</h1>
+      <body>
+
+<div class="container">
+    <h1 class="center">Literature</h1>
+
+    <div class="content">
+        <h2>Poems</h2>
+        <select value={selectedYearPoems} onChange={handlePoemsChange}>
+            <option disabled selected value="">Select Year</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+        </select>
+
+        <h2>Articles</h2>
+        <select>
+            <option disabled selected value="">Select Year</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+        </select>
+    </div>
+</div>
+
+</body>
     </div>
   );
 }
