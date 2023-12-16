683467637116eb9f634a17b5d858d37ce918a567
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-12-16 14:52:06 +0530

    form

--- socse/src/Pages/Contact.js
+++ socse/src/Pages/Contact.js
@@ -3,7 +3,7 @@ import './Home.css';
 function Contact(){
     return (
      <div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2c6h-wfWzBxRpwEtNcyca6GMj5H-iuewu9VgnIuERInkLOQ/viewform?embedded=true" width="640" height="1284" frameborder="0" marginheight="0" marginwidth="0" className="form">Loading…</iframe>
+       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2c6h-wfWzBxRpwEtNcyca6GMj5H-iuewu9VgnIuERInkLOQ/viewform?embedded=true" width="640" height="1142" frameborder="0" marginheight="0" marginwidth="0" className='form'>Loading…</iframe>
      </div>
     );
 }
