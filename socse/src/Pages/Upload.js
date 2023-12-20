a7972f12badf2cabc302b0710ac2cfdf759d3123
vaishnavicg <01fe21bcs271@kletech.ac.in>
2023-12-20 14:32:18 +0530

    login page

--- socse/src/Pages/Upload.js
+++ socse/src/Pages/Upload.js
@@ -2,6 +2,48 @@ import React from 'react'
 
 export default function Upload() {
   return (
-    <div>upload</div>
+    <div>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"/>
+        <title>Login Page</title>
+    </head>
+    <body>
+    
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Login</h4>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="usernameOrEmail">Username or Email</label>
+                                <input type="text" class="form-control" id="usernameOrEmail" name="usernameOrEmail" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" required />
+                            </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.9/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    </body>
+    </html>
+  </div>
   )
 }
