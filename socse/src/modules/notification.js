82167bdf7482c0bccd353c778003ec6d08a0e593
vaishnavicg <01fe21bcs271@kletech.ac.in>
2024-01-06 23:32:33 +0530

    notification

--- socse/src/modules/notification.js
+++ socse/src/modules/notification.js
@@ -1,18 +1,18 @@
+// Notification.js
+
 import React from "react";
 import './notification.css';
 import { useNavigate } from 'react-router-dom';
 
+function Notification() {
+  const navigate = useNavigate();
+  const register = () => navigate('/register');
 
-
-
-function Notification(){
-    const navigate = useNavigate();
-    const register = () => navigate('/register');
-    return (
-     <div className="notify">
-      <a onClick={register}>click here to register for latest events</a>        
-
-     </div>
-    );
+  return (
+    <div className="notify">
+      <a onClick={register}>Click here to register for the latest events</a>
+    </div>
+  );
 }
+
 export default Notification;
