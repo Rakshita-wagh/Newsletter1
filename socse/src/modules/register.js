a3a93e71c9f210e63650e611ed6d56a63a20eee1
vaishnavicg <01fe21bcs271@kletech.ac.in>
2024-01-08 14:18:59 +0530

    add

--- socse/src/modules/register.js
+++ socse/src/modules/register.js
@@ -1,26 +1,10 @@
-import React from "react";
-import './regiser.css';
+// import './Home.css';
 
-function Register(){
-    return ( 
-    <div>
-          <form className="forms">
-          <label for="name">Your name:</label>
-          <input type="text" value="" id="name" name="names" required placeholder=" Name"></input>
-          <br></br>
-          <br></br>
-          <label for="email">Your Email:</label>
-          <input type="email" value="" id="email"  name="emails" required placeholder="Email"></input>
-          <br></br>
-          <br></br>
-          <label for="pass"> Your Password:</label> 
-          <input  type="password" value="" id="pass" name="passes" required placeholder="pass"></input>
-          <br></br>
-          <br></br>
-         <button>Submit</button>
-          </form>
-   </div>
-     
+function register(){
+    return (
+     <div>
+       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd2c6h-wfWzBxRpwEtNcyca6GMj5H-iuewu9VgnIuERInkLOQ/viewform?embedded=true" width="640" height="1142" frameborder="0" marginheight="0" marginwidth="0" className='form'>Loading…</iframe>
+     </div>
     );
 }
-export default Register;
+export default register;
