d5c5a0f8f52bb6231652c6254c09cd9a7baf2112
vaishnavicg <01fe21bcs271@kletech.ac.in>
2024-01-10 19:09:00 +0530

    add

--- api/magazine.js
+++ api/magazine.js
@@ -103,4 +103,4 @@ async function addSectionToPDF(pdfDoc, sectionTitle, sectionData) {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-//hiiii
+//hello how are you
